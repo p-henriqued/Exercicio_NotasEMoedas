49f6aa8f87394b5d55e9be0b4e766d45d673d69f
Pedro <pedrodias3737@gmail.com>
2023-01-11 17:26:27 -0300

    Uptade

--- src/App.java
+++ src/App.java
@@ -1,65 +1,55 @@
-import java.text.DecimalFormat;
+import java.util.Locale;
 import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        DecimalFormat df = new DecimalFormat("0");
-        
         double cash = sc.nextDouble();
+        int notas = (int) cash;
+        int coins = (int) ((cash - notas)*100);
+        sc.close();
         //Notas
-        double note100 = (int)cash/100;
-        double restOfNote100 = cash%100;
-
-        double note50 = (int)restOfNote100/50;
-        double restOfNote50 = restOfNote100 %50;
-        
-        double note20 = (int)restOfNote50/20;
-        double restOfNote20 = restOfNote50 %20;
-
-        double note10 = (int)restOfNote20/10;
-        double restOfNote10 = restOfNote20%10;
-
-        double note5 = (int)restOfNote10/5;
-        double restOfNote5 = restOfNote10%5;
-
-        double note2 = (int)restOfNote5/2;
-        double restOfNote2 = restOfNote5%2;
+        int note100 = notas /100;
+        notas -= note100 * 100;
+        int note50 = notas/50;
+        notas -= note50 *50;
+        int note20 = notas/20;
+        notas -= note20 * 20;
+        int note10 = notas /10;
+        notas -= note10 * 10;
+        int note5 = notas /5;
+        notas -= note5 * 5;
+        int note2 = notas/2;
+        notas -= note2 * 2;
         //Moedas
-        double coin1 = (int)restOfNote2/1;
-        double resCoin1 = restOfNote2%1;
+        int coin1 = notas;
+        notas-= coin1 * 1;
+        int coin50 = coins/50;
+        coins -= coin50 * 50;
+        int coin25 = coins/25;
+        coins -= coin25 * 25;
+        int coin10 = coins/10;
+        coins -= coin10 * 10;
+        int coin5 = coins/5;
+        coins -= coin5 * 5;
+        int coin = coins;
 
-        double coin50 = resCoin1/0.5;
-        double resCoin50 = resCoin1%0.5;
-
-        double coin25 = resCoin50/0.25;
-        double resCoin25 = resCoin50%0.25;
-
-        double coin10 = resCoin25/0.10;
-        double resCoin10 = resCoin25%0.10;  
-        
-        double coin5 = resCoin10/0.5;
-        double resCoin5 = resCoin10%0.5;
-        
-        double coin = resCoin5/0.1;
-
- 
         System.out.println("NOTAS:");
-        System.out.println(df.format(note100)+" notas(s) de R$ 100.00");
-        System.out.println(df.format(note50)+" notas(s) de R$ 50.00");
-        System.out.println(df.format(note20)+" notas(s) de R$ 20.00");
-        System.out.println(df.format(note10)+" notas(s) de R$ 10.00");
-        System.out.println(df.format(note5)+" notas(s) de R$ 5.00");
-        System.out.println(df.format(note2)+" notas(s) de R$ 2.00");
+        System.out.println(note100+" nota(s) de R$ 100.00");
+        System.out.println(note50+" nota(s) de R$ 50.00");
+        System.out.println(note20+" nota(s) de R$ 20.00");
+        System.out.println(note10+" nota(s) de R$ 10.00");
+        System.out.println(note5+" nota(s) de R$ 5.00");
+        System.out.println(note2+" nota(s) de R$ 2.00");
         
         System.out.println("MOEDAS:");
-        System.out.println(df.format(coin1)+" moeda(s) de R$ 1.00");
-        System.out.println(df.format(coin50) + " moeda(s) de R$ 0.50");
-        System.out.println(df.format(coin25)+" moeda(s) de R$ 0.25");
-        System.out.println(df.format(coin10)+" moeda(s) de R$ 0.10");
-        System.out.println(df.format(coin5)+" moeda(s) de R$ 0.05");
-        System.out.println(df.format(coin)+" moeda(s) de R$ 0.01");
+        System.out.println(coin1+" moeda(s) de R$ 1.00");
+        System.out.println(coin50+" moeda(s) de R$ 0.50");
+        System.out.println(coin25+" moeda(s) de R$ 0.25");
+        System.out.println(coin10+" moeda(s) de R$ 0.10");
+        System.out.println(coin5+" moeda(s) de R$ 0.05");
+        System.out.println(coin+" moeda(s) de R$ 0.01");
         
-        sc.close();
+        
     }
 }
